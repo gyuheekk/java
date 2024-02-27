2b4f02edfb7fd3643a6ea45ea79799a6a0509dd8
Gyu <kh010129@naver.com>
2024-02-27 11:01:20 +0900

    first project

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cf/500ce64f76d4001e1199cad35ad6a88f
@@ -0,0 +1,7 @@
+package t01;
+
+class Test3 {
+	public static void main(String[] args) {
+		System.out.println("규희집에 오신 걸 환영합니다.");
+	}
+}
---
+++ Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    pubilc static void main(String[] args) {
+        System.out.println("Hello~~Welcome to java!!");
+    }
+}
